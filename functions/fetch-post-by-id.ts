b267c2f84b275d5c5e955b1f7a7399ef45b1cc8b
NightFeather <leolee50910@gmail.com>
2022-09-25 05:39:40 +0800

    done

---
+++ functions/fetch-post-by-id.ts
@@ -0,0 +1,88 @@
+import { Client } from '@notionhq/client';
+import { NotionToMarkdown } from "notion-to-md";
+
+const { NOTION_SECRET } = process.env;
+const notionClient = new Client(
+  { auth: NOTION_SECRET }
+);
+const notionToMarkdown = new NotionToMarkdown(
+  { notionClient: notionClient }
+);
+
+class PostData {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public thumbnail: string,
+    public createdAt: string,
+    public lastEditedAt: string,
+    public tags: Array<string>,
+    public content: string
+  ) {}
+}
+
+function parseMarkdownFrontmatter(
+  title: string,
+  description: string,
+  thumbnail: string,
+  createdAt: string,
+  lastEditedAt: string,
+  tags: Array<string>
+): string {
+  return `---
+layout: "../../layouts/BlogPost.astro"
+title: "${title}"
+description: "${description}"
+thumbnail: "${thumbnail}"
+createdAt: "${createdAt}"
+lastEditedAt: "${lastEditedAt}"
+tags: ${JSON.stringify(tags)}
+---\n`
+}
+
+function parsePostTags(rawTags: Array<Object>): Array<string> {
+  let result: Array<string> = [];
+  rawTags.forEach((rawTag) => {
+    result.push(
+      JSON.parse(JSON.stringify(rawTag)).name
+    );
+  });
+  return result;
+}
+
+async function fetchPostById(postId: string): Promise<PostData> {
+  let rawPostData = await notionClient.pages.retrieve({ page_id: postId });
+  let postData = JSON.parse(JSON.stringify(rawPostData));
+  let rawMarkdown = await notionToMarkdown.pageToMarkdown(postId);
+  let markdownString = notionToMarkdown.toMarkdownString(rawMarkdown).replace(/^  \<\/details\>$/mg, "</details>").replace(/^  $/mg, "<br/>");
+  let markdownFrontmatter = parseMarkdownFrontmatter(
+    postData.properties.Title.title[0].plain_text,
+    postData.properties.Description.rich_text[0].plain_text,
+    postData.properties.Thumbnail.files[0].external.url,
+    postData.created_time,
+    postData.last_edited_time,
+    parsePostTags(postData.properties.Tags.multi_select)
+  );
+  return new PostData(
+    postId,
+    postData.properties.Title.title[0].plain_text,
+    postData.properties.Description.rich_text[0].plain_text,
+    postData.properties.Thumbnail.files[0].external.url,
+    postData.created_time,
+    postData.last_edited_time,
+    parsePostTags(postData.properties.Tags.multi_select),
+    markdownFrontmatter + markdownString
+  );
+}
+
+import type { Handler } from "@netlify/functions";
+
+const handler: Handler = async (event, _) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(await fetchPostById(event.queryStringParameters!.id ?? "")),
+  };
+};
+
+export { handler };
---
+++ functions/fetch-posts.ts
@@ -0,0 +1,49 @@
+import { Client } from '@notionhq/client';
+import type { Handler } from "@netlify/functions";
+
+const { NOTION_SECRET, DATABASE_ID } = process.env;
+const notionClient = new Client(
+  { auth: NOTION_SECRET }
+);
+
+class PostInfo {
+  constructor(
+    public id: string, 
+    public title: string,
+    public description: string,
+    public thumbnail: string
+  ) {}
+}
+
+async function fetchBlogPosts(): Promise<Array<PostInfo>> {
+  let data = await notionClient.databases.query(
+    {
+      database_id: DATABASE_ID as string
+    }
+  );
+  let result: Array<PostInfo> = [];
+  data.results.forEach(async (rawPostData) => {
+    let postData = JSON.parse(JSON.stringify(rawPostData));
+    if (!postData.properties.Public.checkbox) {
+      return;
+    }
+    result.push(
+      new PostInfo(
+        postData.id,
+        postData.properties.Title.title[0].plain_text,
+        postData.properties.Description.rich_text[0].plain_text,
+        postData.properties.Thumbnail.files[0].external.url
+      )
+    );
+  });
+  return result;
+}
+
+const handler: Handler = async (_, __) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(await fetchBlogPosts()),
+  };
+};
+
+export { handler };
